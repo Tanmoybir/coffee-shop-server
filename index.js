3e4b7edcf801b14c073da5c6a7df9be30d7e0ca6
Tanmoy Bir <tanmoy7468@gmail.com>
2023-12-27 20:52:49 +0600

    dotenv file added safe use for database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -8,10 +9,10 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-// coffeeMaster
-// Hl7DiGSnPMfKJNvE
+// 
+// 
 
-const uri = "mongodb+srv://coffeeMaster:Hl7DiGSnPMfKJNvE@cluster0.xmlybhe.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xmlybhe.mongodb.net/?retryWrites=true&w=majority`  ;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
